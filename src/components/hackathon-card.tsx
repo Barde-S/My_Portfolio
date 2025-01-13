7a9ff2d1aa228f04ae8da1f8ce346c5efe25bf3b
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2025-01-13 19:20:33 +0100

    Update hackathon-card.tsx

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -2,17 +2,19 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 
+interface LinkItem {
+  icon: React.ReactNode;
+  title: string;
+  href: string;
+}
+
 interface Props {
   title: string;
   description: string;
   dates: string;
   location: string;
   image?: string;
-  links?: readonly {
-    icon: React.ReactNode;
-    title: string;
-    href: string;
-  }[];
+  links?: readonly LinkItem[];
 }
 
 export function HackathonCard({
@@ -25,31 +27,32 @@ export function HackathonCard({
 }: Props) {
   return (
     <li className="relative ml-10 py-4">
+      {/* Avatar */}
       <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
         <Avatar className="border size-12 m-auto">
           <AvatarImage src={image} alt={title} className="object-contain" />
-          <AvatarFallback>{title[0]}</AvatarFallback>
+          <AvatarFallback>{title.charAt(0)}</AvatarFallback>
         </Avatar>
       </div>
+
+      {/* Main Content */}
       <div className="flex flex-1 flex-col justify-start gap-1">
-        {dates && (
-          <time className="text-xs text-muted-foreground">{dates}</time>
-        )}
+        {dates && <time className="text-xs text-muted-foreground">{dates}</time>}
         <h2 className="font-semibold leading-none">{title}</h2>
-        {location && (
-          <p className="text-sm text-muted-foreground">{location}</p>
-        )}
+        {location && <p className="text-sm text-muted-foreground">{location}</p>}
         {description && (
-          <span className="prose dark:prose-invert text-sm text-muted-foreground">
+          <p className="prose dark:prose-invert text-sm text-muted-foreground">
             {description}
-          </span>
+          </p>
         )}
       </div>
-      {links && links.length > 0 && (
-        <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
-          {links?.map((link, idx) => (
-            <Link href={link.href} key={idx}>
-              <Badge key={idx} title={link.title} className="flex gap-2">
+
+      {/* Links */}
+      {links?.length > 0 && (
+        <div className="mt-2 flex flex-wrap items-start gap-2">
+          {links.map((link, idx) => (
+            <Link href={link.href} key={idx} passHref>
+              <Badge title={link.title} className="flex items-center gap-2">
                 {link.icon}
                 {link.title}
               </Badge>
